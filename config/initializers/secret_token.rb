11a3536df42aa7136db58a1f9faf49967d73ad5b
sherpc <aleksandrsher@gmail.com>
2012-04-03 21:07:19 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VkToGmail::Application.config.secret_token = '00e77f74e30510d2c1961957e8a08b2046fc9643dc880be10430ea0323025b967550f4eda0f13546d09c1db6c0f50c0c46ef8c884fb30989625ee086b7935437'
